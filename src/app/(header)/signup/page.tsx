842a4c263d7bcf59a537aa03d9a654c7e9847107
changwoo-yu <changwooyu1213@gmail.com>
2024-08-04 03:21:34 +0900

    feat: fetch 커스텀훅, API route 정의 테스트

--- src/app/(header)/signup/page.tsx
+++ src/app/(header)/signup/page.tsx
@@ -1,6 +1,20 @@
 "use client";
+
+import useFetch from "@/app/hooks";
+import { useState } from "react";
+
 const Signup = () => {
-  return <div>회원가입 페이지</div>;
+  const { data, loading, error } = useFetch("/api");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Fetched Data</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default Signup;
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 export async function GET(request: Request) {
-  
-  const res = await fetch(`https://naver.com`);
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
+  console.log(data);
 
   return NextResponse.json({ data });
 }
 
-// const { searchParams } = new URL(request.url); 
+// const { searchParams } = new URL(request.url);
 // const page = searchParams.get("page");
